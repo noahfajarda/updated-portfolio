53775cfe8a3a87caec2d47e266e3fa39b04da8dd
Noah Fajarda <noahfajarda@gmail.com>
2023-06-23 16:58:02 -0700

    added tailwind module

--- src/utils/simplifiedTailwind.js
+++ src/utils/simplifiedTailwind.js
@@ -1,3 +1,4 @@
 const customTransition = "transition-all ease-in-out duration-300"
+const customGradientText = "bg-clip-text text-transparent bg-gradient-to-t from-pink-500 to-violet-500";
 
-export { customTransition }
+export { customTransition, customGradientText }
